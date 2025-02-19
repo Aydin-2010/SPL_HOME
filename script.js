4f168a355babccac8cc0e77aac3b84cb3b2f9a04
Aydin-2010 <aydinabdullahaziz@gmail.com>
2025-02-19 20:46:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+const element = document.getElementById("extra");
+for(let i = 0; i < 100 ; i++){
+  let li = document.createElement("li");
+  li.textContent = "hi" + i;
+  element.appendChild(li);
+} 
