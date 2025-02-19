59479933a70099f48f31a252d9c86ecdc224d16c
Aydin-2010 <aydinabdullahaziz@gmail.com>
2025-02-19 20:33:02 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-let ul = document.getElementById("extra");
 
-for (let i = 1; i <= 1000; i++) {  // Adding 10 list items
-    let li = document.createElement("li");
-    li.textContent = "Item " + i;
-    ul.appendChild(li);
-}
